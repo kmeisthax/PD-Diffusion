1704f22fd28f7ecbfe66f2bdf3f23c69ddb1d507
David Wendt <dcrkid@yahoo.com>
2022-10-27 22:59:21 -0400

    Store crawled images and titles

--- PDDiffusion/datasets/WikimediaCommons/__main__.py
+++ PDDiffusion/datasets/WikimediaCommons/__main__.py
@@ -1,16 +1,27 @@
-from PDDiffusion.WikimediaCommons import Connection, PD_EXPIRATION_CATEGORY
+from PDDiffusion.datasets.WikimediaCommons import Connection, PD_EXPIRATION_CATEGORY
+from urllib.request import urlopen
+import os.path, json
 
 conn = Connection()
 
 for item in conn.walk_category(PD_EXPIRATION_CATEGORY, member_types=["file"]):
     print(item["title"])
+
+    localpath = os.path.join("sets", "wikimedia")
+    localfile = os.path.join(localpath, item["title"].removeprefix("File:"))
+
+    if not os.path.exists(localpath):
+        os.makedirs(localpath)
     
     image_info = conn.image_info(titles=[item["title"]], iiprop=["url"])
     for image in image_info["query"]["pages"][str(item["pageid"])]["imageinfo"]:
-        print (image["url"])
+        with urlopen(image["url"]) as source:
+            with open(localfile, "wb") as sink:
+                sink.write(source.read())
     
     page_terms = conn.page_terms(titles=[item["title"]])
     if "terms" in page_terms["query"]["pages"][str(item["pageid"])]:
-        for (key, values) in page_terms["query"]["pages"][str(item["pageid"])]["terms"].items():
-            for value in values:
-                print("{0}: {1}".format(key, value))
+        with open(localfile + '.json', 'w') as sink:
+            sink.write(json.dumps(page_terms["query"]["pages"][str(item["pageid"])]["terms"]))
+    
+    #TODO: Retain category information when crawling.
