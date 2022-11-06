9faf7744dfd8160f0f49024c2e41d2c0a095e027
David Wendt <dcrkid@yahoo.com>
2022-11-06 18:55:51 -0500

    Restructure the training loop to allow responding to CUDA out-of-memory errors.

--- PDDiffusion/train/__main__.py
+++ PDDiffusion/train/__main__.py
@@ -3,7 +3,7 @@ from PDDiffusion.test import evaluate
 
 import os.path, torch, math, json
 
-from accelerate import Accelerator
+from accelerate import Accelerator, find_executable_batch_size
 from diffusers import DDPMScheduler, DDPMPipeline
 from diffusers.optimization import get_cosine_schedule_with_warmup
 from diffusers.hub_utils import init_git_repo, push_to_hub
@@ -15,27 +15,12 @@ config.dataset_name = "pd-diffusion-wikimedia"
 
 dataset = load_dataset(config)
 
-train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=config.train_batch_size, shuffle=True)
-
 if not os.path.exists("output"):
     os.makedirs("output")
 
 os.chdir("output")
 
-(model, progress) = load_model_and_progress(config)
-print("Restarting after epoch {}".format(progress["last_epoch"]))
-
-noise_scheduler = DDPMScheduler(num_train_timesteps=1000)
-
-optimizer = torch.optim.AdamW(model.parameters(), lr=config.learning_rate)
-
-lr_scheduler = get_cosine_schedule_with_warmup(
-    optimizer=optimizer,
-    num_warmup_steps=config.lr_warmup_steps,
-    num_training_steps=(len(train_dataloader) * config.num_epochs),
-)
-
-def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler):
+def train_loop(config):
     # Initialize accelerator and tensorboard logging
     accelerator = Accelerator(
         mixed_precision=config.mixed_precision,
@@ -47,67 +32,89 @@ def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_s
         if config.push_to_hub:
             repo = init_git_repo(config, at_init=True)
         accelerator.init_trackers("train_example")
+
+    @find_executable_batch_size(starting_batch_size=config.train_batch_size)
+    def inner_training_loop(batch_size):
+        nonlocal accelerator
+        accelerator.free_memory()
+
+        train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+        (model, progress) = load_model_and_progress(config)
+        print("Restarting after epoch {}".format(progress["last_epoch"]))
+
+        optimizer = torch.optim.AdamW(model.parameters(), lr=config.learning_rate)
+
+        lr_scheduler = get_cosine_schedule_with_warmup(
+            optimizer=optimizer,
+            num_warmup_steps=config.lr_warmup_steps,
+            num_training_steps=(len(train_dataloader) * config.num_epochs),
+        )
+
+        noise_scheduler = DDPMScheduler(num_train_timesteps=1000)
+        
+        # Prepare everything
+        # There is no specific order to remember, you just need to unpack the 
+        # objects in the same order you gave them to the prepare method.
+        model, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
+            model, optimizer, train_dataloader, lr_scheduler
+        )
+        
+        global_step = 0
+
+        # Now you train the model
+        last_epoch = progress["last_epoch"] + 1 + config.num_epochs
+        for epoch in range(progress["last_epoch"] + 1, last_epoch):
+            progress_bar = tqdm(total=len(train_dataloader), disable=not accelerator.is_local_main_process)
+            progress_bar.set_description(f"Epoch {epoch}")
+
+            for step, batch in enumerate(train_dataloader):
+                clean_images = batch['images']
+                # Sample noise to add to the images
+                noise = torch.randn(clean_images.shape).to(clean_images.device)
+                bs = clean_images.shape[0]
+
+                # Sample a random timestep for each image
+                timesteps = torch.randint(0, noise_scheduler.num_train_timesteps, (bs,), device=clean_images.device).long()
+
+                # Add noise to the clean images according to the noise magnitude at each timestep
+                # (this is the forward diffusion process)
+                noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
+                
+                with accelerator.accumulate(model):
+                    # Predict the noise residual
+                    noise_pred = model(noisy_images, timesteps, return_dict=False)[0]
+                    loss = F.mse_loss(noise_pred, noise)
+                    accelerator.backward(loss)
+
+                    accelerator.clip_grad_norm_(model.parameters(), 1.0)
+                    optimizer.step()
+                    lr_scheduler.step()
+                    optimizer.zero_grad()
+                
+                progress_bar.update(1)
+                logs = {"loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0], "step": global_step}
+                progress_bar.set_postfix(**logs)
+                accelerator.log(logs, step=global_step)
+                global_step += 1
+
+            # After each epoch you optionally sample some demo images with evaluate() and save the model
+            if accelerator.is_main_process:
+                pipeline = DDPMPipeline(unet=accelerator.unwrap_model(model), scheduler=noise_scheduler)
+
+                if (epoch + 1) % config.save_image_epochs == 0 or epoch == last_epoch - 1:
+                    evaluate(config, epoch, pipeline)
+
+                if (epoch + 1) % config.save_model_epochs == 0 or epoch == last_epoch - 1:
+                    if config.push_to_hub:
+                        push_to_hub(config, pipeline, repo, commit_message=f"Epoch {epoch}", blocking=True)
+                    else:
+                        pipeline.save_pretrained(config.output_dir)
+
+                        with open(os.path.join(config.output_dir, "progress.json"), "w") as progress_file:
+                            progress["last_epoch"] = epoch
+                            json.dump(progress, progress_file)
     
-    # Prepare everything
-    # There is no specific order to remember, you just need to unpack the 
-    # objects in the same order you gave them to the prepare method.
-    model, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
-        model, optimizer, train_dataloader, lr_scheduler
-    )
-    
-    global_step = 0
+    inner_training_loop()
 
-    # Now you train the model
-    last_epoch = progress["last_epoch"] + 1 + config.num_epochs
-    for epoch in range(progress["last_epoch"] + 1, last_epoch):
-        progress_bar = tqdm(total=len(train_dataloader), disable=not accelerator.is_local_main_process)
-        progress_bar.set_description(f"Epoch {epoch}")
-
-        for step, batch in enumerate(train_dataloader):
-            clean_images = batch['images']
-            # Sample noise to add to the images
-            noise = torch.randn(clean_images.shape).to(clean_images.device)
-            bs = clean_images.shape[0]
-
-            # Sample a random timestep for each image
-            timesteps = torch.randint(0, noise_scheduler.num_train_timesteps, (bs,), device=clean_images.device).long()
-
-            # Add noise to the clean images according to the noise magnitude at each timestep
-            # (this is the forward diffusion process)
-            noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
-            
-            with accelerator.accumulate(model):
-                # Predict the noise residual
-                noise_pred = model(noisy_images, timesteps, return_dict=False)[0]
-                loss = F.mse_loss(noise_pred, noise)
-                accelerator.backward(loss)
-
-                accelerator.clip_grad_norm_(model.parameters(), 1.0)
-                optimizer.step()
-                lr_scheduler.step()
-                optimizer.zero_grad()
-            
-            progress_bar.update(1)
-            logs = {"loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0], "step": global_step}
-            progress_bar.set_postfix(**logs)
-            accelerator.log(logs, step=global_step)
-            global_step += 1
-
-        # After each epoch you optionally sample some demo images with evaluate() and save the model
-        if accelerator.is_main_process:
-            pipeline = DDPMPipeline(unet=accelerator.unwrap_model(model), scheduler=noise_scheduler)
-
-            if (epoch + 1) % config.save_image_epochs == 0 or epoch == last_epoch - 1:
-                evaluate(config, epoch, pipeline)
-
-            if (epoch + 1) % config.save_model_epochs == 0 or epoch == last_epoch - 1:
-                if config.push_to_hub:
-                    push_to_hub(config, pipeline, repo, commit_message=f"Epoch {epoch}", blocking=True)
-                else:
-                    pipeline.save_pretrained(config.output_dir)
-
-                    with open(os.path.join(config.output_dir, "progress.json"), "w") as progress_file:
-                        progress["last_epoch"] = epoch
-                        json.dump(progress, progress_file)
-
-train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler)
+train_loop(config)
