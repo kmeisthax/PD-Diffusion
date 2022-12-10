0d15ade81833af26ff695d33b717e56b75a479f1
David Wendt <dcrkid@yahoo.com>
2022-12-10 17:22:51 -0500

    Rudimentary emulation of the technique template.
    There are so many random things you can do with this template that I would rather not get into right now

--- PDDiffusion/datasets/WikimediaCommons/wikiparse.py
+++ PDDiffusion/datasets/WikimediaCommons/wikiparse.py
@@ -459,6 +459,30 @@ def extract_template_tag(subtmpl, warn=False, preferred_lang="en"):
                     print_warn(f"Unknown portrait of template parameter {name_text}")
 
         text_value = f"Portrait of {text_value}"
+    elif subtmpl_title.lower() == "technique":
+        text_value = ""
+
+        for part in subtmpl.findall("part"):
+            name = part.find("name")
+            value = part.find("value")
+
+            name_text = name.text
+            if (name.attrib is not None and "index" in name.attrib):
+                name_text = name.attrib["index"]
+            elif name_text is None:
+                name_text = ""
+            else:
+                name_text = name_text.strip()
+
+            if name_text == "1":
+                text_value += extract_text_from_value(value, warn=warn, preferred_lang="en")
+            elif name_text == "and":
+                text_value += " and " + extract_text_from_value(value, warn=warn, preferred_lang="en")
+            elif name_text == "2" or name_text == "on":
+                text_value += " on " + extract_text_from_value(value, warn=warn, preferred_lang="en")
+            else:
+                if warn:
+                    print_warn(f"Unsupported technique template parameter {name_text}")
     elif subtmpl_title.lower() == "portrait of a woman":
         text_value = "Portrait of a woman"
     elif subtmpl_title.lower() == "madonna and child":
