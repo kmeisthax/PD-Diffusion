0adc4621175f8cc4e4302492a5bab3f27b80dcff
David Wendt <dcrkid@yahoo.com>
2022-12-13 21:05:47 -0500

    Factor out the scrape function

--- PDDiffusion/datasets/WikimediaCommons/__init__.py
+++ PDDiffusion/datasets/WikimediaCommons/__init__.py
@@ -129,6 +129,84 @@ def image_is_valid(file):
         print ("Warning: Image {} could not be read from disk, error was: {}".format(file, e))
         return False
 
+def scrape_and_save_metadata(conn, item, localfile):
+    """Scrape data from the Wikimedia connection and item to the local file path given.
+    
+    Item must be a dict with "title" and "pageid" keys matching the item to download.
+    Localfile must be a valid filesystem path.
+    
+    Returns True if the file scraped successfully and new data was obtained.
+    False indicates one of the following possible error conditions:
+    
+     - The file is already available locally and is up-to-date
+     - The file cannot be saved because it exceeds PIL's size requirements
+    
+    If you are enforcing a scrape limit, be sure not to count failures against
+    that limit."""
+    metadatafile = localfile + '.json'
+
+    if os.path.exists(localfile + ".banned") or os.path.exists(localfile + ".bannedmetadata"):
+        #Skip downloaded files that were banned from the training set.
+        #Images can be banned either because they were too large to decode,
+        #or because the file could not be decoded in PIL.
+        return False
+
+    file_already_exists = os.path.exists(localfile)
+    metadata_already_exists = os.path.exists(metadatafile)
+    if metadata_already_exists:
+        #Check if we need to re-scrape categories or wikitext
+        with open(metadatafile, 'r') as source:
+            source_data = json.load(source)
+
+            if "categories" not in source_data or "parsetree" not in source_data:
+                metadata_already_exists = False
+    
+    if file_already_exists and metadata_already_exists:
+        return False
+    
+    print(item["title"])
+    
+    image_info = conn.image_info(titles=[item["title"]], iiprop=["url", "size"])["query"]["pages"][str(item["pageid"])]["imageinfo"]
+    image_is_banned = False
+    for image in image_info:
+        if image["size"] > Image.MAX_IMAGE_PIXELS:
+            #Don't even download the file, just mark the metadata as banned
+            metadatafile = localfile + '.bannedmetadata'
+            image_is_banned = True
+            break
+
+        if not file_already_exists:
+            with conn.urlopen(image["url"]) as source:
+                with open(localfile, "wb") as sink:
+                    sink.write(source.read())
+            
+            if not image_is_valid(localfile):
+                os.rename(localfile, localfile + ".banned")
+                image_is_banned = True
+    
+    if not metadata_already_exists:
+        metadata = {}
+        metadata["title"] = item["title"].removeprefix("File:").removesuffix(".jpg").removesuffix(".jpeg").removesuffix(".png").removesuffix(".tif").removesuffix(".tiff")
+
+        page_terms = conn.page_terms(titles=[item["title"]])
+        if "terms" in page_terms["query"]["pages"][str(item["pageid"])]:
+            metadata["terms"] = page_terms["query"]["pages"][str(item["pageid"])]["terms"]
+        
+        page_cats = conn.categories(titles=[item["title"]])
+        if "categories" in page_cats["query"]["pages"][str(item["pageid"])]:
+            metadata["categories"] = page_cats["query"]["pages"][str(item["pageid"])]["categories"]
+        
+        page_text = conn.parse_tree(item["title"])
+        if "parsetree" in page_text["parse"]:
+            metadata["parsetree"] = page_text["parse"]["parsetree"]
+        
+        metadata["imageinfo"] = image_info
+        
+        with open(metadatafile, 'w') as sink:
+            sink.write(json.dumps(metadata))
+    
+    return not image_is_banned
+
 def local_wikimedia(limit = None, prohibited_categories=[], load_images=True, intended_maximum_size=512):
     """Load in training data previously downloaded by running this module's main.
     
@@ -244,4 +322,4 @@ def local_wikimedia(limit = None, prohibited_categories=[], load_images=True, in
 
             yield {"image": image, "image_file_path": os.path.abspath(file), "text": label}
         else:
-            yield {"text": label}
+            yield {"image_file_path": os.path.abspath(file), "text": label}
--- PDDiffusion/datasets/WikimediaCommons/__main__.py
+++ PDDiffusion/datasets/WikimediaCommons/__main__.py
@@ -1,4 +1,4 @@
-from PDDiffusion.datasets.WikimediaCommons import Connection, DEFAULT_UA, BASE_API_ENDPOINT, PD_ART_CATEGORY_OLD100, LOCAL_STORAGE, image_is_valid
+from PDDiffusion.datasets.WikimediaCommons import Connection, DEFAULT_UA, BASE_API_ENDPOINT, PD_ART_CATEGORY_OLD100, LOCAL_STORAGE, scrape_and_save_metadata
 from urllib.request import urlopen
 import os.path, json, sys
 from PIL import Image
@@ -11,6 +11,7 @@ class WikimediaScrapeOptions:
     limit:int = field(metadata={"help": "How many new images to download"}, default=200)
     endpoint:str = field(metadata={"help": "Which Wiki mirror to scrape from, default is Wikimedia Commons"}, default=BASE_API_ENDPOINT)
     ua:str = field(metadata={"args": ["--user-agent"], "help": "Set the user-agent string for all requests"}, default=DEFAULT_UA)
+    rescrape:str = field(metadata={"args": ["--rescrape"], "help": "Redownload existing metadata or files if changed"})
 
 options = WikimediaScrapeOptions.parse_args(sys.argv[1:])
 
@@ -27,67 +28,6 @@ for item in conn.walk_category(PD_ART_CATEGORY_OLD100, member_types=["file"]):
 
     sanitized_image_name = item["title"].removeprefix("File:").replace("\"", "").replace("'", "").replace("?", "").replace("!", "").replace("*", "").strip()
     localfile = os.path.join(LOCAL_STORAGE, sanitized_image_name)
-    metadatafile = localfile + '.json'
-
-    if os.path.exists(localfile + ".banned") or os.path.exists(localfile + ".bannedmetadata"):
-        #Skip downloaded files that were banned from the training set.
-        #Images can be banned either because they were too large to decode,
-        #or because the file could not be decoded in PIL.
-        continue
-
-    file_already_exists = os.path.exists(localfile)
-    metadata_already_exists = os.path.exists(metadatafile)
-    if metadata_already_exists:
-        #Check if we need to re-scrape categories or wikitext
-        with open(metadatafile, 'r') as source:
-            source_data = json.load(source)
-
-            if "categories" not in source_data or "parsetree" not in source_data:
-                metadata_already_exists = False
     
-    if file_already_exists and metadata_already_exists:
-        continue
-    
-    print(item["title"])
-    
-    image_info = conn.image_info(titles=[item["title"]], iiprop=["url", "size"])["query"]["pages"][str(item["pageid"])]["imageinfo"]
-    image_is_banned = False
-    for image in image_info:
-        if image["size"] > Image.MAX_IMAGE_PIXELS:
-            #Don't even download the file, just mark the metadata as banned
-            metadatafile = localfile + '.bannedmetadata'
-            image_is_banned = True
-            break
-
-        if not file_already_exists:
-            with conn.urlopen(image["url"]) as source:
-                with open(localfile, "wb") as sink:
-                    sink.write(source.read())
-            
-            if not image_is_valid(localfile):
-                os.rename(localfile, localfile + ".banned")
-                image_is_banned = True
-    
-    if not metadata_already_exists:
-        metadata = {}
-        metadata["title"] = item["title"].removeprefix("File:").removesuffix(".jpg").removesuffix(".jpeg").removesuffix(".png").removesuffix(".tif").removesuffix(".tiff")
-
-        page_terms = conn.page_terms(titles=[item["title"]])
-        if "terms" in page_terms["query"]["pages"][str(item["pageid"])]:
-            metadata["terms"] = page_terms["query"]["pages"][str(item["pageid"])]["terms"]
-        
-        page_cats = conn.categories(titles=[item["title"]])
-        if "categories" in page_cats["query"]["pages"][str(item["pageid"])]:
-            metadata["categories"] = page_cats["query"]["pages"][str(item["pageid"])]["categories"]
-        
-        page_text = conn.parse_tree(item["title"])
-        if "parsetree" in page_text["parse"]:
-            metadata["parsetree"] = page_text["parse"]["parsetree"]
-        
-        metadata["imageinfo"] = image_info
-        
-        with open(metadatafile, 'w') as sink:
-            sink.write(json.dumps(metadata))
-    
-    if not image_is_banned:
+    if scrape_and_save_metadata(conn, item, localfile):
         count += 1
