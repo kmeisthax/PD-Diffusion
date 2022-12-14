211c115a4bf8de8e91183eec5e2a4df3e459a0dd
David Wendt <dcrkid@yahoo.com>
2022-12-13 19:19:50 -0500

    Add a CSV export along with the test results JSON.

--- PDDiffusion/datasets/WikimediaCommons/extract_test.py
+++ PDDiffusion/datasets/WikimediaCommons/extract_test.py
@@ -1,6 +1,6 @@
 from PDDiffusion.datasets.WikimediaCommons.wikiparse import extract_information_from_wikitext
 from PDDiffusion.datasets.WikimediaCommons import LOCAL_STORAGE
-import itertools, glob, os, json, sys
+import itertools, glob, os, json, csv
 
 report = []
 
@@ -38,8 +38,13 @@ for file in itertools.chain(
                         "message": str(e)
                     }
             
+            terms = None
+            if "terms" in metadata:
+                terms = metadata["terms"]
+            
             report.append({
                 "file": file,
+                "terms": terms,
                 "status": "present",
                 "parsetree": parses
             })
@@ -51,3 +56,40 @@ for file in itertools.chain(
 
 with open("extract_test_results.json", 'w') as json_file:
     json.dump(report, json_file, indent=4)
+
+headers = ["File", "object type", "artist", "date", "title", "medium", "description", "__label"]
+headermap = {key: id for (id, key) in enumerate(headers)}
+
+rows = []
+
+for item in report:
+    if item["status"] == "present":
+        item_data = {"File": item["file"]}
+
+        if item["terms"] is not None:
+            for key in item["terms"].keys():
+                item_data["__" + key] = ", ".join(item["terms"][key])
+
+        item_data.update(item["parsetree"]["*"]["en_only"])
+
+        for key in item_data.keys():
+            if key not in headers:
+                key_id = len(headers)
+                headers.append(key)
+                headermap[key] = key_id
+        
+        item_row = [""] * len(headers)
+        
+        for key in headermap.keys():
+            if key in item_data:
+                item_row[headermap[key]] = item_data[key]
+        
+        rows.append(item_row)
+
+with open("extracted_data.csv", 'w', newline='', encoding="utf-8") as data_file:
+    writer = csv.writer(data_file)
+
+    writer.writerow(headers)
+    
+    for item in rows:
+        writer.writerow(item)
