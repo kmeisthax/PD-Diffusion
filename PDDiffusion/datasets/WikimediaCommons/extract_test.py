2a3156806e757c6e189fa0c91d6ce7c6848a5876
David Wendt <dcrkid@yahoo.com>
2022-12-26 11:02:46 -0500

    Remove flat-file dependency from the extract test

--- PDDiffusion/datasets/WikimediaCommons/extract_test.py
+++ PDDiffusion/datasets/WikimediaCommons/extract_test.py
@@ -1,57 +1,57 @@
+"""Test extracting data from stored wikitext."""
+
+import os, json, csv
 from PDDiffusion.datasets.WikimediaCommons.wikiparse import extract_information_from_wikitext
-from PDDiffusion.datasets.WikimediaCommons import LOCAL_STORAGE
-import itertools, glob, os, json, csv
+from PDDiffusion.datasets.WikimediaCommons.model import WikimediaCommonsImage
+from PDDiffusion.datasets.model import File
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 report = []
 
-for file in itertools.chain(
-        glob.iglob(os.path.join(LOCAL_STORAGE, "*.jpg")),
-        glob.iglob(os.path.join(LOCAL_STORAGE, "*.jpeg")),
-        glob.iglob(os.path.join(LOCAL_STORAGE, "*.png")),
-        glob.iglob(os.path.join(LOCAL_STORAGE, "*.tif")),
-        glob.iglob(os.path.join(LOCAL_STORAGE, "*.tiff"))
-    ):
-    
-    if os.path.exists(file + ".json"):
+load_dotenv()
+engine = create_engine(os.getenv("DATABASE_CONNECTION"), future=True)
+
+with Session(engine) as session:
+    for (image, article) in WikimediaCommonsImage.select_all_image_articles(session):
+        if image.file.storage_provider != File.LOCAL_FILE:
+            print(f"Non-local file provider {image.file.storage_provider}")
+
+        file = image.file.url
+        metadata = article.wikidata
+        
         #Get around the fact that input redirection appears to change text encoding.
         print(file.encode("utf-8"))
 
-        with open(file + ".json", 'r') as metadata_file:
-            metadata = json.load(metadata_file)
+        parses = {}
 
-            parses = {}
+        for name in metadata["parsetree"]:
+            xmlstr = metadata["parsetree"][name]
 
-            for name in metadata["parsetree"]:
-                xmlstr = metadata["parsetree"][name]
+            try:
+                extracted = extract_information_from_wikitext(xmlstr, warn=True, preferred_lang=None)
 
-                try:
-                    extracted = extract_information_from_wikitext(xmlstr, warn=True, preferred_lang=None)
-
-                    parses[name] = {
-                        "status": "valid",
-                        "data": extracted,
-                        "en_only": extract_information_from_wikitext(xmlstr, warn=True, preferred_lang="en")
-                    }
-                except Exception as e:
-                    parses[name] = {
-                        "status": "exception",
-                        "message": str(e)
-                    }
-            
-            terms = None
-            if "terms" in metadata:
-                terms = metadata["terms"]
-            
-            report.append({
-                "file": file,
-                "terms": terms,
-                "status": "present",
-                "parsetree": parses
-            })
-    else:
+                parses[name] = {
+                    "status": "valid",
+                    "data": extracted,
+                    "en_only": extract_information_from_wikitext(xmlstr, warn=True, preferred_lang="en")
+                }
+            except Exception as e:
+                parses[name] = {
+                    "status": "exception",
+                    "message": str(e)
+                }
+        
+        terms = None
+        if "terms" in metadata:
+            terms = metadata["terms"]
+        
         report.append({
             "file": file,
-            "status": "missing"
+            "terms": terms,
+            "status": "present",
+            "parsetree": parses
         })
 
 with open("extract_test_results.json", 'w') as json_file:
