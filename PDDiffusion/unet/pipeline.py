e8f57aa82f50cb86b61df4e4fc809de9e72815a3
David Wendt <dcrkid@yahoo.com>
2023-01-05 07:47:14 -0500

    Apparently the deprecation of this parameter got pushed forward

--- PDDiffusion/unet/pipeline.py
+++ PDDiffusion/unet/pipeline.py
@@ -118,7 +118,7 @@ class DDPMConditionalPipeline(DiffusionPipeline):
             "Please make sure to instantiate your scheduler with `prediction_type` instead. E.g. `scheduler ="
             " DDPMScheduler.from_pretrained(<model_id>, prediction_type='epsilon')`."
         )
-        predict_epsilon = deprecate("predict_epsilon", "0.10.0", message, take_from=kwargs)
+        predict_epsilon = deprecate("predict_epsilon", "0.12.0", message, take_from=kwargs)
 
         if predict_epsilon is not None:
             new_config = dict(self.scheduler.config)
