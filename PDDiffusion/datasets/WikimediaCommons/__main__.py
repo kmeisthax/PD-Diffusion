ba96dc52f158c344302de3f491b0d8a5d7578b4f
David Wendt <dcrkid@yahoo.com>
2023-04-29 23:47:41 -0400

    Add new scraping options.
    `--rescrape` now no longer implies `--update`; you may `--rescrape` a category instead of all downloaded images. Rescraping a category will still download unknown images like normal.
    
    File and subcategory downloads have to be enabled with the `--files` and `--subcategories` flags.
    
    Finally, you can now pick what category to scrape with `--category`.

--- PDDiffusion/datasets/WikimediaCommons/__main__.py
+++ PDDiffusion/datasets/WikimediaCommons/__main__.py
@@ -11,11 +11,14 @@ from sqlalchemy.orm import Session
 @dataclass
 class WikimediaScrapeOptions:
     email:str = field(metadata={"help": "E-mail address to attach to all requests, e.g. --email test@example.com"})
-    limit:int = field(metadata={"help": "How many new images to download"}, default=200)
+    limit:int = field(metadata={"help": "How many new images to download"}, default=None)
     endpoint:str = field(metadata={"help": "Which Wiki mirror to scrape from, default is Wikimedia Commons"}, default=BASE_API_ENDPOINT)
     ua:str = field(metadata={"args": ["--user-agent"], "help": "Set the user-agent string for all requests"}, default=DEFAULT_UA)
-    update:bool = field(metadata={"args": ["--update"], "help": "Update outdated or damaged images"}, default=False)
-    rescrape:bool = field(metadata={"args": ["--rescrape"], "help": "Redownload existing metadata"}, default=False)
+    update:bool = field(metadata={"args": ["--update"], "help": "Check already-downloaded files for updates. (Incompatible with subcategory storage.)"}, default=False)
+    rescrape:bool = field(metadata={"args": ["--rescrape"], "help": "Redownload metadata for already-downloaded files and categories"}, default=False)
+    files:bool = field(metadata={"args": ["--files"], "help": "Download files that are children of the given category"}, default=False)
+    subcategories:bool = field(metadata={"args": ["--subcategories"], "help": "Store all categories traversed when downloading images. This is also necessary for category exclusion or weighting during dataset export."}, default=False)
+    category:str = field(metadata={"args": ["--category"], "help": "Change the starting category to scrape from"}, default=PD_ART_CATEGORY_OLD100)
 
 options = WikimediaScrapeOptions.parse_args(sys.argv[1:])
 
@@ -35,14 +38,32 @@ with Session(engine) as session:
     if session.execute(select(Dataset).where(Dataset.id == dataset_id)).one_or_none() is None:
         session.add(Dataset(id=dataset_id))
 
-    if options.rescrape or options.update:
+    if options.update:
+        if options.subcategories:
+            raise Exception("Update and subcategory scrape are mutually exclusive options")
+        
         source = WikimediaCommonsImage.select_all_image_articles(session)
     else:
-        source = conn.walk_category(PD_ART_CATEGORY_OLD100, member_types=["file"])
+        member_types = []
+        if options.files:
+            member_types.append("file")
+        
+        if options.subcategories:
+            member_types.append("subcat")
+
+        source = conn.walk_category(options.category, member_types=member_types)
+    
+    if options.subcategories:
+        count += scrape_and_save_metadata(conn, session, pages=[options.category], force_rescrape=options.rescrape)
     
     for pages in chunked_iterable(source, 10):
-        if count >= options.limit:
+        if options.limit is not None and count >= options.limit:
             break
 
-        count += scrape_and_save_metadata(conn, session, pages=pages, force_rescrape=options.rescrape)
-        session.commit()
+        this_count = scrape_and_save_metadata(conn, session, pages=pages, force_rescrape=options.rescrape)
+        if this_count > 0:
+            session.commit()
+        
+        count += this_count
+    
+    session.commit()
